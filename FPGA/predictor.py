a7bb2be7d14f710ad5ee782e6f9b9c1aa511b029
nishanthelango <nishanthelango@yahoo.com.sg>
2021-10-11 19:12:00 +0800

    update MLP params

--- FPGA/predictor.py
+++ FPGA/predictor.py
@@ -7,9 +7,9 @@ class Predictor():
 
     def __init__(self):
         # Initialize pynq Overlay to program FPGA using bitstream
-        self.overlay = Overlay(
-            '/home/xilinx/jupyter_notebooks/MLP_accelerator/MLP5.bit')
+        self.overlay = Overlay('MLP.bit')
         self.accelerate_mlp = self.overlay.accelerate_MLP_0
+        self.DANCE_TO_NUM_MAP = {0: 'dab', 1: 'jamesbond', 2: 'mermaid'}
 
     # Axilite MMIO interface does not support read/write of floats
     def float_to_integer(self, f):
@@ -30,12 +30,12 @@ class Predictor():
         #          bit 3  - ap_ready (Read)
         #          bit 7  - auto_restart (Read/Write)
         #          others - reserved
-
+        # 0x0800 ~
+        # 0x0fff : Memory 'inputs' (480 * 32b)
+        #          Word n : bit [31:0] - inputs[n]
         # 0x1000 ~
-        # 0x1fff : Memory 'input_r' (561 * 32b)
-        #          Word n : bit [31:0] - input_r[n]
-        # 0x2000 ~
-        # 0x203f : Memory 'output_r' (12 * 32b)
+        # 0x100f : Memory 'outputs' (3 * 32b)
+        #          Word n : bit [31:0] - outputs[n]
         #          Word n : bit [31:0] - output_r[n]
 
         INPUT_OFFSET = 0x1000
@@ -45,8 +45,8 @@ class Predictor():
         AP_DONE = 2
         # Each input is 32 bits = 4 bytes
         NUM_BYTES = 4
-        INPUT_LENGTH = 561
-        OUTPUT_LENGTH = 12
+        INPUT_LENGTH = 480
+        OUTPUT_LENGTH = 3
 
         # Write input values to IP
         for i in range(INPUT_LENGTH):
@@ -67,4 +67,4 @@ class Predictor():
             result.append(self.integer_to_float(res))
 
         # Return label with highest value
-        return result.index(max(result)) + 1
+        return DANCE_TO_NUM_MAP[result.index(max(result))]
