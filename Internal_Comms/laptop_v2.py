3ab84d7a3f583a1eea22139ad3866e9ec51841af
harithadiv <--global>
2021-11-01 20:17:21 +0800

    final version of data collection

--- Internal_Comms/laptop_v2.py
+++ Internal_Comms/laptop_v2.py
@@ -10,27 +10,28 @@ service_uuid = "0000dfb0-0000-1000-8000-00805f9b34fb"
 #BEETLE_0 = "b0:b1:13:2d:b3:1a"
 #BEETLE_1 = "b0:b1:13:2d:d4:86"
 #BEETLE_2 = "b0:b1:13:2d:d7:97"
-#BEETLE_0 = "b0:b1:13:2d:b4:19"
+BEETLE_0 = "b0:b1:13:2d:b4:19"
 #BEETLE_3 = "b0:b1:13:2d:b4:7d"
-BEETLE_6 = "b0:b1:13:2d:b5:13"
+#BEETLE_6 = "b0:b1:13:2d:b5:13"
 
+#>>>>>>>>>>Setup Data collection>>>>>>>>
+address = [BEETLE_0]
+csv_file_name = "dab_haritha_1"
+
+csv_time = 0
 Connect_Header = "++++++++++++++++++++++++++++++++++++++++++++++++++++"
 Disconnect_Header = "----------------------------------------------------"
 Data_Header = ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
 Reconnect_Header = "####################################################"
 newline = "\n"
 
- 
-csv_time = 0
-address = [BEETLE_6]
-global_delegate = []
-[global_delegate.append(0) for idx in range(len(address))]
 address_map = {}
 disconnect_map = {}
 handshake_map = {}
 start_time = {}
 buffer_map = {} 
 train_data = []
+
 for i in range(len(address)):
     address_map[address[i]] = i
 
@@ -47,8 +48,8 @@ for i in range(len(address)):
     handshake_map[address[i]] = False
 
 
+
 def main():
-    #BLE = Setup.scan()
     for addr in address:
         count = 0
         start = datetime.now()
@@ -74,14 +75,9 @@ def main():
                 print(newline)
                 main_thread(beetle, address_map[addr]).start()
             continue
-
-
-    #print("\n")
-    #print("Formed peripheral with ", no_of_beetles, " beetles")
     
 def checksum_ack(packet):
     check = packet[0] ^ packet[1] ^ packet[2] ^ packet[3] ^ packet[4] ^ packet[5] ^ packet[6] ^ packet[7] ^ packet[8] ^ packet[9]
-    print(check)
     if check == packet[10]:
         return True
     return False
@@ -104,7 +100,6 @@ def checksum_direction(packet):
     if check == packet[11]:
         return True
     return False
-
 
 
 class MyDelegate(btle.DefaultDelegate):
@@ -154,7 +149,6 @@ class MyDelegate(btle.DefaultDelegate):
                 if(correct_size):
                     buffer_map[addr] =  b""
                     packet_type = struct.unpack("!b", packet[:1])[0]
-                    #print(packet_type)
 
                     if packet_type == 1:
                         packet = struct.unpack("<bhhhhhhhhbh", packet)
@@ -170,15 +164,11 @@ class MyDelegate(btle.DefaultDelegate):
                     elif packet_type == 2 and handshake_map[addr] == True:
                         packet = struct.unpack("<bhhhihhbi", packet)
                         global csv_time
-                        #print(Data_Header)
-                        #print("Receiving data from beetle ID: ", i)
                         if(checksum_imu(packet)):
                             #print("Checksum correct!")
                             #print(packet)
                             #print(Data_Header)
                             #print(newline)
-                            #print(time.time() - csv_time)
-                            #csv_time = time.time()
                             #if(packet[7] == 0):
                                 #print("isDancing = 0")
                             #elif(packet[7] == 1):
@@ -186,7 +176,6 @@ class MyDelegate(btle.DefaultDelegate):
 
                             if (len(train_data) < 3100):
                                 real_data = packet[1:10]
-                                #print("real data:", real_data)
                                 train_data.append(list(real_data))
 
                         elif(not checksum_imu(packet)):
@@ -194,11 +183,10 @@ class MyDelegate(btle.DefaultDelegate):
 
                         if (len(train_data) == 1200):
                             print("reached CSV")
-                            #print(train_data)
                             print(type(real_data))
                             print(time.time() - csv_time)
 
-                            with open("test.csv", "w", newline="") as f:
+                            with open(csv_file_name, "w", newline="") as f:
                                 writer = csv.writer(f)
                                 writer.writerows(train_data)
                                             
@@ -212,9 +200,6 @@ class MyDelegate(btle.DefaultDelegate):
                         print(packet)
                         print(Data_Header)
                         print(newline)
-
-                        #print(packet)
-
 
                     elif packet_type == 4 and handshake_map[addr] == True:
                         packet = struct.unpack("<bbbhhhhhhhbh", packet)
@@ -232,22 +217,6 @@ class MyDelegate(btle.DefaultDelegate):
 
 
 
-                        
-
-
-                
-
-
-                #packetType = struct.unpack("!b", data[:1])[0]
-                #print(len(data))
-                #print(packetType)
-                #print(data)
-            #packet = struct.unpack('!c', data)         
-            #print("Notif received")
-            #print(cHandle)
-            #print(packet[0])
-
-
 class Setup:
     def setPeripheral(address):
         try:
@@ -257,8 +226,6 @@ class Setup:
             return 1
         return beetle
         
-    
-
     def scan():
         count = 0
         print("Scanning for devices...")
@@ -271,7 +238,6 @@ class Setup:
         return devices
 
 
-
 class main_thread(threading.Thread):
     def __init__(self, beetle, ID):
         threading.Thread.__init__(self)
@@ -280,15 +246,11 @@ class main_thread(threading.Thread):
         self.serial_service = beetle.getServiceByUUID(service_uuid)
         self.serial_char = self.serial_service.getCharacteristics()[0]
 
-
     def run(self):
         global csv_time
         handshake_map[self.beetle.addr] = False
         try:
             self.beetle.setDelegate(MyDelegate(self.beetle.addr))
-            #print(self.beetle.setDelegate(MyDelegate(self.beetle.addr)))
-            global_delegate[self.ID] = self.beetle.setDelegate(MyDelegate(self.beetle.addr))
-            #print(global_delegate[self.ID])
             self.handshake()
             if (handshake_map[self.beetle.addr]):
                 csv_time = time.time()
@@ -307,7 +269,6 @@ class main_thread(threading.Thread):
 
                     #Handling disconnections
                     else:
-                        #print(time.time(), start_time[self.beetle.addr])
                         if (time.time() - start_time[self.beetle.addr] > 15):
                             print(newline)
                             print(Disconnect_Header)
@@ -318,8 +279,6 @@ class main_thread(threading.Thread):
                             self.reconnect()
 
                     continue
-    
-
             else:
                 print(newline)
                 print(Disconnect_Header)
@@ -340,7 +299,6 @@ class main_thread(threading.Thread):
             self.beetle.disconnect()
             self.reconnect()
 
-
     def handshake(self):
         self.serial_char.write(bytes("r", "utf-8"), withResponse = False)
         sleep(1)
@@ -352,15 +310,11 @@ class main_thread(threading.Thread):
         print(Connect_Header)
         print(newline)
         
-
         #three-way handshake
         if self.beetle.waitForNotifications(2.0):
             handshake_end = time.time()
-            #print(handshake_end - handshake_start)
-            #print("Successfully made connection with beetle ID ", self.ID)
             if (handshake_map[self.beetle.addr]):
                 self.serial_char.write(bytes("a", "utf-8"), withResponse = False)
-
 
     def reconnect(self):
         sleep(1)
