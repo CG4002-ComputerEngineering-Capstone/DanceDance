52c6c5833a236fe3e9850f8f532402357f8a0e76
nishanthelango <nishanthelango@yahoo.com.sg>
2021-11-14 22:40:32 +0800

    Update FPGA files

--- FPGA/README.md
+++ FPGA/README.md
@@ -1,1 +1,16 @@
 # FPGA #
+
+An MLP Neural Network hardware accelerator implemented using C++ in Vivado HLS. An Axilite MMIO interface is used to handle reading and writing of data between Ultra96 CPU and FPGA.
+
+
+### Files ###
+
+MLP_accelerator.cpp: MLP model implemented in Vivado HLS C++
+
+MLP_accelerator_test.cpp: C++ testbench for simulation
+
+MLP_axilite.ipynb: Jupyter notebook for testing accuracy of FPGA model and recording ultra96 power usage
+
+predictor.py: Pynq driver script to process live data using FPGA and return predicted output 
+
+
--- FPGA/predictor.py
+++ FPGA/predictor.py
@@ -1,15 +1,21 @@
 from pynq import Overlay
 import struct
 import pynq
-
+from numpy import exp
 
 class Predictor():
 
     def __init__(self):
         # Initialize pynq Overlay to program FPGA using bitstream
-        self.overlay = Overlay('MLP.bit')
+        self.overlay = Overlay('MLP11.bit')
         self.accelerate_mlp = self.overlay.accelerate_MLP_0
-        self.DANCE_TO_NUM_MAP = {0: 'dab', 1: 'jamesbond', 2: 'mermaid'}
+        self.NUM_TO_DANCE_MAP = {0: 'dab', 1: 'jamesbond', 2: 'mermaid', 
+                                3: 'scarecrow', 4:'pushback',  5: 'cowboy', 
+                                6: 'window360', 7: 'snake', 8: 'logout'}
+    
+    def softmax(self, vector):
+        e = exp(vector)
+        return e / e.sum()
 
     # Axilite MMIO interface does not support read/write of floats
     def float_to_integer(self, f):
@@ -23,30 +29,30 @@ class Predictor():
         # Mapping for MMIO obtained from Vivado HLS
 
         # AXILiteS
-        # 0x0000 : Control signals
+        # 0x000 : Control signals
         #          bit 0  - ap_start (Read/Write/COH)
         #          bit 1  - ap_done (Read/COR)
         #          bit 2  - ap_idle (Read)
         #          bit 3  - ap_ready (Read)
         #          bit 7  - auto_restart (Read/Write)
         #          others - reserved
-        # 0x0800 ~
-        # 0x0fff : Memory 'inputs' (480 * 32b)
-        #          Word n : bit [31:0] - inputs[n]
-        # 0x1000 ~
-        # 0x100f : Memory 'outputs' (3 * 32b)
-        #          Word n : bit [31:0] - outputs[n]
+
+        # 0x400 ~
+        # 0x7ff : Memory 'input_r' (561 * 32b)
+        #          Word n : bit [31:0] - input_r[n]
+        # 0x800 ~
+        # 0x83f : Memory 'output_r' (12 * 32b)
         #          Word n : bit [31:0] - output_r[n]
 
-        INPUT_OFFSET = 0x1000
-        OUTPUT_OFFSET = 0x2000
-        CONTROL_OFFSET = 0x0000
+        INPUT_OFFSET = 0x400
+        OUTPUT_OFFSET = 0x800
+        CONTROL_OFFSET = 0x000
         AP_START = 1
         AP_DONE = 2
         # Each input is 32 bits = 4 bytes
         NUM_BYTES = 4
-        INPUT_LENGTH = 480
-        OUTPUT_LENGTH = 3
+        INPUT_LENGTH = 210
+        OUTPUT_LENGTH = 9
 
         # Write input values to IP
         for i in range(INPUT_LENGTH):
@@ -65,6 +71,11 @@ class Predictor():
         for i in range(OUTPUT_LENGTH):
             res = self.accelerate_mlp.read(OUTPUT_OFFSET + i*NUM_BYTES)
             result.append(self.integer_to_float(res))
+        
+        result = list(self.softmax(result))
 
+        print(f'Prediction results:')
+        print(f'dab, jamesbond, mermaid, scarecrow, pushback, cowboy, window360, snake, logout')
+        print(result)
         # Return label with highest value
-        return DANCE_TO_NUM_MAP[result.index(max(result))]
+        return self.NUM_TO_DANCE_MAP[result.index(max(result))] if max(result) > 0.9 else 'idle'
